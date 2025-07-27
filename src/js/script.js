8465b37e51d08f1100c149c88d7c1718363e8235
Cayo Maia <chenrycksm@gmail.com>
2025-07-27 17:54:59 -0300

    feat: update main image, name and description on character hover
    Atualiza dinamicamente a imagem principal, nome e descrição com base no personagem selecionado. Também adiciona scroll para o topo em dispositivos com largura menor que 450px.

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,24 @@ console.log(characters);
 
 characters.forEach((character) => {
     character.addEventListener('mouseenter', () => {
-        
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        };
+
         const characterSelected = document.querySelector('.selected');
         characterSelected.classList.remove('selected');
 
         character.classList.add('selected');
 
+        const characterBigImage = document.querySelector('.character-image');
+        const idCharacter = character.attributes.id.value;
+        characterBigImage.src = `/src/imagens/card-${idCharacter}.png`;
 
+        const characterName = document.getElementById('character-name');
+        characterName.innerText = character.getAttribute('data-name');
+
+        const characterDescription = document.getElementById('character-description');
+        characterDescription.innerText = character.getAttribute('data-description');
     });
-    
 });
